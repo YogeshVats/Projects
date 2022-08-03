ab063889286890ee00647f5b031a5d7b3ea836fe
Yogesh Vats <yogesh.vatz@gmail.com>
2022-08-03 16:25:47 +0530

    Add comments

--- FirstFlaskApp/app.py
+++ FirstFlaskApp/app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask    # flask is a repo and Flask is a class
 
 
-app = Flask(__name__)
+app = Flask(__name__)      # creating flask app and giving it a specific name using __name__
 
-@app.route('/')     # 'http://www.google.com/'
-def home():
+@app.route('/')     # 'http://www.google.com/'  Home route
+def home():                 # name of this method doesn't matter and can be anything
     print("Hello World!")
     return "Hello World!"
 
 
-app.run(port = 5000)
+app.run(port = 5000)        # running flask application and giving it port 5000
