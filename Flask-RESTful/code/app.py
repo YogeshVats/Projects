9dcb0e963283b43080c8c17823cb6e0c154a202b
Yogesh Vats <yogesh.vatz@gmail.com>
2022-08-08 12:14:14 +0530

    Rename resource

--- Flask-RESTful/code/app.py
+++ Flask-RESTful/code/app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 api = Api(app)
 
 # Api works with resources and every resource has to be a class
-class Student(Resource):
+class Item(Resource):
     def get(self, name):
         return {'student': name}
 
-api.add_resource(Student, '/student/<string:name>')     # http://127.0.0.1:5000/student/Rolf
+api.add_resource(Item, '/item/<string:name>')     # http://127.0.0.1:5000/item/<name>
 
 app.run(port=5000)
